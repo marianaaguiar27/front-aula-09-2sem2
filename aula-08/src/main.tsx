489cb5d41a87b23cbdaea022da61863a1549d7c9
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-07 11:33:24 -0300

    feat : Passagem de parâmetros com useParams
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- aula-08/src/main.tsx
+++ aula-08/src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([{
     {path: "/produtos", element: <Produtos/>},
     {path: "/editar-produtos/:id", element: <EditarProdutos/>},  
   ]
-}]);
+}]); 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- aula-08/src/routes/EditarProdutos/index.tsx
+++ aula-08/src/routes/EditarProdutos/index.tsx
@@ -1,7 +1,17 @@
+import { useParams } from "react-router-dom"
+
 export default function EditarProdutos() {
+
+  //Realizando um destructuring para pegar o id da url:
+  const { id } = useParams();
+  console.log(`ID do produto a ser editado: ${id}`);
+
+
   return (
     <main>
         <h1>Editar Produtos</h1>
+        <p>ID do produto: {id}</p>
     </main>
   )
 }
+ 
